9e360295049aea9fe0e84834612679acbec390a1
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-15 20:18:32 +0700

    checkpoint

--- front_end_portofolio/src/components/allItem.jsx
+++ front_end_portofolio/src/components/allItem.jsx
@@ -21,13 +21,15 @@ class AllItem extends React.Component {
 			<div class="container">
 				<div className="row">
 					{list_product.map((isi,i)=>(
-						<div className="col-md-4 kotak_barang">
-						<div className="col-md-12">
-							<Link className="underlineHover" to={`/product/${isi.id}`}><img style={{borderRadius:"10px"}} src={isi.gambar_1} className="fotoBarang" alt=""/></Link>
+						<div className="col-md-4">
+							<div className="kotak_barang">
+								<div className="col-md-12">
+									<Link className="underlineHover" to={`/product/${isi.id}`}><img style={{borderRadius:"10px"}} src={isi.gambar_1} className="fotoBarang" alt=""/></Link>
+								</div>
+								<div className="col-md-12" style={{paddingBottom:"10px",paddingTop:"10px"}}><span>Rp {isi.price}</span></div>
+								<div className="col-md-12"><span>{isi.item_name}</span></div>
+							</div>
 						</div>
-						<div className="col-md-12" style={{paddingBottom:"10px",paddingTop:"10px"}}><span>Rp {isi.price}</span></div>
-					<div className="col-md-12"><span>{isi.item_name}</span></div>
-					</div>
 					))}
 				</div>
 			</div>
