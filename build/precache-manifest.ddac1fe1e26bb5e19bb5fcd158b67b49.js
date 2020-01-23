d41b8e634c2599050ca72b234ca58ff861db4010
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-23 20:39:00 +0700

    travis

---
+++ build/precache-manifest.ddac1fe1e26bb5e19bb5fcd158b67b49.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fc741427e4d43e57df077ace83e64d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba1f35f09d68f28a4850",
+    "url": "/static/css/main.4735f6ef.chunk.css"
+  },
+  {
+    "revision": "55851c9a7e47a68a3629",
+    "url": "/static/js/2.98b1cd2c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.98b1cd2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba1f35f09d68f28a4850",
+    "url": "/static/js/main.8606d9f0.chunk.js"
+  },
+  {
+    "revision": "8f5d98502e0829ad2c63",
+    "url": "/static/js/runtime-main.c9f392ec.js"
+  },
+  {
+    "revision": "93810770007bcf20b3800e07f0a059ef",
+    "url": "/static/media/dribbble.93810770.png"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/static/media/facebook.5d6ea38a.png"
+  },
+  {
+    "revision": "d18a1b4d27375bc5d2bf5d8579389fbd",
+    "url": "/static/media/home_icon5.d18a1b4d.png"
+  },
+  {
+    "revision": "0bf67f0212a381a8bd2b0b75991e8de5",
+    "url": "/static/media/linkedIn.0bf67f02.png"
+  },
+  {
+    "revision": "21ff64e5e5ccf85f4999be070149bb11",
+    "url": "/static/media/logoM.21ff64e5.png"
+  },
+  {
+    "revision": "1c97c064695555eff6f4428855f5447f",
+    "url": "/static/media/patrick.1c97c064.png"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/static/media/profile_icon2.7ea4af7d.png"
+  },
+  {
+    "revision": "538203c846f0771cbedf796c7afd4605",
+    "url": "/static/media/slider1.538203c8.jpg"
+  },
+  {
+    "revision": "1a02308ccf56278beb7154293f3ee35c",
+    "url": "/static/media/slider2.1a02308c.jpg"
+  },
+  {
+    "revision": "1bbb4a7c74c758265501781d694e1084",
+    "url": "/static/media/slider3.1bbb4a7c.jpg"
+  },
+  {
+    "revision": "874012267741d39a0357a4250a85938c",
+    "url": "/static/media/sliderNoMovement1.87401226.jpg"
+  },
+  {
+    "revision": "80211f83e351139210ce0a0ae80c5006",
+    "url": "/static/media/sliderNoMovement2.80211f83.jpg"
+  }
+]);
