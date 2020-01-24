3652291a8d7396364dfe2239ab8cbad7f88fbf06
Hamdi Ranuharja <hamdi@alterra.id>
2020-01-24 08:48:14 +0700

    trial travis1

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # fill the env file
 #sed -i -e 's|REAK_STAGING_SONAR_URL|'"${REAK_STAGING_SONAR_URL}"'|g' docker-staging.env
 
-sudo docker build -f "Dockerfile" -t ajisepulsa/alta:frontend-$TRAVIS_BUILD_NUMBER  --label "Maintaner  Bayu Aji <aji@alterra.id>" .
+sudo docker build -f "Dockerfile" -t hamdiranu/containerd:frontend-$TRAVIS_BUILD_NUMBER  --label "Maintaner  Hamdi R <hamdi@alterra.id>" .
 # push apps image to docker hub
-sudo docker push ajisepulsa/alta:frontend-$TRAVIS_BUILD_NUMBER
+sudo docker push hamdiranu/containerd:frontend-$TRAVIS_BUILD_NUMBER
 
 # go inside kubernetes Server
 sed -i -e 's|KUBE_STAGING_CA_CERT|'"${KUBE_STAGING_CA_CERT}"'|g' kubeconfig
@@ -14,4 +14,4 @@ sed -i -e 's|AWS_STAGING_ACCESS_KEY|'"${AWS_STAGING_ACCESS_KEY}"'|g' ~/.aws/cred
 sed -i -e 's|AWS_STAGING_SECRET_KEY|'"${AWS_STAGING_SECRET_KEY}"'|g' ~/.aws/credentials
 
 # update apps deployment
-kubectl -n coba --kubeconfig kubeconfig set image deployment/${APP_K8S_DEPLOYMENT_NAME} ${APP_K8S_CONTAINER_NAME}=hamdiranu/containerd:frontend-${TRAVIS_BUILD_NUMBER}
+kubectl -n alta9 --kubeconfig kubeconfig set image deployment/${APP_K8S_DEPLOYMENT_NAME} ${APP_K8S_CONTAINER_NAME}=hamdiranu/containerd:frontend-${TRAVIS_BUILD_NUMBER}
